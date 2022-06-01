ce760b869f33b1acde93b996ae27c6e239eb225a
blablass <82516957+blablass@users.noreply.github.com>
2022-06-01 17:41:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 nosex = "";
 nosey = "";
+img1 = "";
 function preload(){
-
+    img1 =loadImage("https://i.postimg.cc/X7N2ym4r/download-removebg-preview.png")
 }
 function setup(){
     canvas = createCanvas(800,800)
     canvas.center()
     webcam = createCapture(VIDEO)
+    webcam.size(800,800)
     webcam.hide()
     posenet = ml5.poseNet(webcam,modelloaded)
     posenet.on("pose",gotposes)
@@ -17,9 +19,6 @@ function modelloaded(){
 function takepic(){
     save("myimg.png")
 }
-function draw(){
-    image(webcam,0,0,800,800)
-}
 function gotposes(results){
     if(results.length > 0){
         console.log(results)
@@ -27,4 +26,11 @@ function gotposes(results){
         nosey = results[0].pose.nose.y
         console.log(nosex,nosey)
     }
+}
+function draw(){
+    image(webcam,0,0,800,800)
+    fill("red")
+    stroke("blue")
+    circle(nosex,nosey,50)
+    image(img1,nosex - 200,nosey - 200,400,300)
 }
